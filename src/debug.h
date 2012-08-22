0b2d2ed561f6cde68fcf584dc8e3a4c5bb8ef83e
Thomas Wickham <mackwic@gmail.com>
2012-08-22 15:41:07 +0200

    separation de l'header de test et de debug

---
+++ src/debug.h
@@ -0,0 +1,34 @@
+#ifndef _DEBUG_
+#define _DEBUG_
+
+#ifdef _DEBUG
+
+#include <stdio.h.h>
+
+#define DEBUG(args...) printf(args)
+#define DEBUGN(args...) printf(args"\n")
+
+#define DEBUG_DO(something) something
+#define DEBUG_DO_START if (1)
+#define DEBUG_DO_END }
+
+#define DEBUG_SET_INT(name, value) int name = value
+#define DEBUG_SET_S_INT(name, value) static int name = value
+#define DEBUG_SET_STRING(name, value) char * name = "value"
+
+#else
+
+#define DEBUG(args...)
+#define DEBUGN(args...)
+
+#define DEBUG_DO(something)
+#define DEBUG_DO_START if (0)
+#define DEBUG_DO_END }
+
+#define DEBUG_SET_INT(name, value)
+#define DEBUG_SET_S_INT(name, value)
+#define DEBUG_SET_STRING(name, value)
+
+#endif
+
+#endif
--- src/test.h
+++ src/test.h
@@ -1,33 +1,6 @@
 #ifndef _TEST_
 #define _TEST_
 
-#ifdef _DEBUG
-
-#define DEBUG(args...) printf(args)
-#define DEBUGN(args...) printf(args"\n")
-
-#define DEBUG_DO(something) something
-#define DEBUG_DO_START if (1)
-#define DEBUG_DO_END }
-
-#define DEBUG_SET_INT(name, value) int name = value
-#define DEBUG_SET_S_INT(name, value) static int name = value
-#define DEBUG_SET_STRING(name, value) char * name = "value"
-
-#else
-
-#define DEBUG(args...)
-#define DEBUGN(args...)
-
-#define DEBUG_DO(something)
-#define DEBUG_DO_START if (0)
-#define DEBUG_DO_END }
-
-#define DEBUG_SET_INT(name, value)
-#define DEBUG_SET_S_INT(name, value)
-#define DEBUG_SET_STRING(name, value)
-
-#endif
 
 
 #ifdef _TEST
